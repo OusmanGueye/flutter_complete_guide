7aef790281233d9965cbde97fee7591b50b38235
Ousmane Gueye <ousmaneg186@gmail.com>
2023-02-27 22:17:26 +0000

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void _answerQuestion() => {
+        setState(() {
+          questionIndex++;
+        }),
+        print('questionIndex = ' + questionIndex.toString())
+      };
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'is your favorite color',
+      'What\'is your favorite animals'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            Question(
+              questions.elementAt(questionIndex),
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
